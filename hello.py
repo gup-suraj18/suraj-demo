15282cd3b5615b7394a303cffe19ff067a5be24e
gup-suraj18 <guptasuraj1504@gmail.com>
2024-08-13 22:25:43 -0700

    create new fun

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Radhe Radhe")
+
+def fun():
+   num1 = int(input('Enter The 1st Number :'))
+   num2 = int(input('Enter The 2nd Number :'))
+
+   c = num1+num2
+
+   print(f"c : {c}")
+
+fun()
